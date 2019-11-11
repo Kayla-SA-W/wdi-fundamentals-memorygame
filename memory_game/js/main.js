125334f2daba66a8ee8818f21e26be944ad53842
Kayla Williams <kaylawilliams@Kaylas-MacBook-Pro.local>
2019-11-11 11:18:53 -0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,24 @@
 
 //array holding all cards
-var cards = ['queen', 'queen', 'king', 'king'];
+var cards = [
+	{
+		rank: "queen",
+		suit: "hearts",
+		cardImage: "images/queen-of-hearts.png"
+	}, {
+		rank: "queen",
+		suit: "diamonds",
+		cardImage: "images/queen-of-diamonds.png"
+	}, {
+		rank: "king",
+		suit: "hearts",
+		cardImage: "images/king-of-hearts.png"
+	}, {
+		rank: "king",
+		suit: "diamonds",
+		cardImage: "images/king-of-diamonds.png"
+	}
+];
 //array for cards the user flipped
 var cardsInPlay = [];
 
@@ -15,8 +33,10 @@ function checkForMatch(){
 	}
 }
 function flipCard(cardId){
-	console.log("User flipped " + cards[cardId] );
-	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped " + cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
+	cardsInPlay.push(cards[cardId].rank);
 	checkForMatch();
 }
 flipCard(0);
